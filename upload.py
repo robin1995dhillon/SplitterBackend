dd6e2a376c1d46b65c993beb76ec6d516b8dfb08
Robinder Jasdev Singh Dhillon <rb802397@dal.ca>
2021-03-31 19:10:36 +0530

    reading form

--- upload.py
+++ upload.py
@@ -1,23 +1,24 @@
 import os
-import boto3
-from jproperties import Properties
+# import boto3
+# from jproperties import Properties
 from flask import Flask, render_template, request, redirect, session, flash
 from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'D://Dalhousie'
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-config = Properties()
-region_name = "us-east-1"
-with open('aws.properties', 'rb') as read:
-    config.load(read)
-
-aws_access_key_id = config.get("AWS_ACCESS_KEY_ID").data
-aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY").data
-aws_session_token = config.get("AWS_SESSION_TOKEN").data
-
-print(aws_access_key_id)
-print(aws_secret_access_key)
-print(aws_session_token)
+# config = Properties()
+# region_name = "us-east-1"
+# with open('aws.properties', 'rb') as read:
+#     config.load(read)
+#
+# aws_access_key_id = config.get("AWS_ACCESS_KEY_ID").data
+# aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY").data
+# aws_session_token = config.get("AWS_SESSION_TOKEN").data
+#
+# print(aws_access_key_id)
+# print(aws_secret_access_key)
+# print(aws_session_token)
 
 
 # dynamo = boto3.resource('dynamodb', region_name=region_name, aws_access_key_id = aws_access_key_id,aws_secret_access_key = aws_secret_access_key,aws_session_token=aws_session_token)
@@ -35,6 +36,6 @@ def getDetails():
             return redirect(request.url)
         if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
             print('done')
-    return
+    return "its done"
