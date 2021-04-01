490fd044238ee5d4c19efad447fc050908a20be6
Robinder Jasdev Singh Dhillon <rb802397@dal.ca>
2021-04-01 16:07:38 +0530

    s3 upload

--- upload.py
+++ upload.py
@@ -1,24 +1,25 @@
 import os
-# import boto3
-# from jproperties import Properties
+import boto3
+from jproperties import Properties
 from flask import Flask, render_template, request, redirect, session, flash
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'D://Dalhousie'
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-# config = Properties()
-# region_name = "us-east-1"
-# with open('aws.properties', 'rb') as read:
-#     config.load(read)
-#
-# aws_access_key_id = config.get("AWS_ACCESS_KEY_ID").data
-# aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY").data
-# aws_session_token = config.get("AWS_SESSION_TOKEN").data
-#
-# print(aws_access_key_id)
-# print(aws_secret_access_key)
-# print(aws_session_token)
+config = Properties()
+region_name = "us-east-1"
+with open('aws.properties', 'rb') as read:
+    config.load(read)
+
+aws_access_key_id = config.get("AWS_ACCESS_KEY_ID").data
+aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY").data
+aws_session_token = config.get("AWS_SESSION_TOKEN").data
+
+print(aws_access_key_id)
+print(aws_secret_access_key)
+print(aws_session_token)
 
 
 # dynamo = boto3.resource('dynamodb', region_name=region_name, aws_access_key_id = aws_access_key_id,aws_secret_access_key = aws_secret_access_key,aws_session_token=aws_session_token)
@@ -36,6 +37,15 @@ def getDetails():
             return redirect(request.url)
         if file:
             filename = secure_filename(file.filename)
+            path = 'D://Dalhousie//' + filename
             file.save(os.path.join(UPLOAD_FOLDER, filename))
+            s3 = boto3.resource(service_name='s3',
+                                region_name=region_name,
+                                aws_access_key_id=aws_access_key_id,
+                                aws_secret_access_key=aws_secret_access_key,
+                                aws_session_token=aws_session_token)
+            s3.Bucket('mp3filebucket').upload_file(Filename=path, Key=filename)
+            print(filename)
+            print(email)
             print('done')
     return "its done"
